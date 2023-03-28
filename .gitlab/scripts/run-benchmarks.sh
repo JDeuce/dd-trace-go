91c43e1631963bdbb350a55cf49823de32b89888
Dmytro Yurchenko <dmytro.yurchenko@datadoghq.com>
2023-03-28 09:57:13 +0200

    Debug perf diff in 5a392f3c

--- .gitlab/scripts/run-benchmarks.sh
+++ .gitlab/scripts/run-benchmarks.sh
@@ -16,11 +16,8 @@ git clone --branch "$CANDIDATE_BRANCH" https://github.com/DataDog/dd-trace-go "$
 cd "$CANDIDATE_SRC/ddtrace/tracer/"
 go test -run=XXX -bench $BENCHMARK_TARGETS -benchmem -count 10 -benchtime 2s ./... | tee "${ARTIFACTS_DIR}/pr_bench.txt"
 
-BASELINE_BRANCH=$(github-find-merge-into-branch --for-repo="$CI_PROJECT_NAME" --for-pr="$CANDIDATE_BRANCH" || :)
-
 if [ ! -z "$BASELINE_BRANCH" ]; then
   cd "$CANDIDATE_SRC"
-  BASELINE_COMMIT_SHA=$(git merge-base "origin/$BASELINE_BRANCH" "origin/$CANDIDATE_BRANCH")
 
   # Clone baseline release
   git clone --branch "$BASELINE_BRANCH" https://github.com/DataDog/dd-trace-go/ "$BASELINE_SRC" && \
