73301f12fbb13c2bae91aaa07a4252c62a9eca4c
Dmytro Yurchenko <dmytro.yurchenko@datadoghq.com>
2023-03-28 15:46:24 +0200

    Change order of execution

--- .gitlab/scripts/run-benchmarks.sh
+++ .gitlab/scripts/run-benchmarks.sh
@@ -7,17 +7,6 @@ source ./.gitlab/scripts/config-benchmarks.sh
 CANDIDATE_BRANCH=$CI_COMMIT_REF_NAME
 CANDIDATE_COMMIT_SHA=$CI_COMMIT_SHA
 
-# Clone candidate release
-git clone --branch "$CANDIDATE_BRANCH" https://github.com/DataDog/dd-trace-go "$CANDIDATE_SRC" && \
-  cd "$CANDIDATE_SRC" && \
-  git checkout $CANDIDATE_COMMIT_SHA
-
-# Run benchmarks for candidate release
-cd "$CANDIDATE_SRC/ddtrace/tracer/"
-#go test -run=XXX -bench $BENCHMARK_TARGETS -benchmem -count 10 -benchtime 2s ./... | tee "${ARTIFACTS_DIR}/pr_bench.txt"
-go test -c -run=XXX -bench "BenchmarkConcurrentTracing" -benchmem -count 10 -benchtime 2s ./...
-cp tracer.test "${ARTIFACTS_DIR}/pr.tracer.test"
-
 if [ ! -z "$BASELINE_BRANCH" ]; then
   cd "$CANDIDATE_SRC"
 
@@ -28,7 +17,15 @@ if [ ! -z "$BASELINE_BRANCH" ]; then
 
   # Run benchmarks for baseline release
   cd "$BASELINE_SRC/ddtrace/tracer/"
-  go test -c -run=XXX -bench "BenchmarkConcurrentTracing" -benchmem -count 10 -benchtime 2s ./...
-  cp tracer.test "${ARTIFACTS_DIR}/main.tracer.test"
-  #go test -run=XXX -bench $BENCHMARK_TARGETS -benchmem -count 10 -benchtime 2s ./... | tee "${ARTIFACTS_DIR}/main_bench.txt"
+  go test -run=XXX -bench $BENCHMARK_TARGETS -benchmem -count 10 -benchtime 2s ./... | tee "${ARTIFACTS_DIR}/main_bench.txt"
 fi
+
+
+# Clone candidate release
+git clone --branch "$CANDIDATE_BRANCH" https://github.com/DataDog/dd-trace-go "$CANDIDATE_SRC" && \
+  cd "$CANDIDATE_SRC" && \
+  git checkout $CANDIDATE_COMMIT_SHA
+
+# Run benchmarks for candidate release
+cd "$CANDIDATE_SRC/ddtrace/tracer/"
+go test -run=XXX -bench $BENCHMARK_TARGETS -benchmem -count 10 -benchtime 2s ./... | tee "${ARTIFACTS_DIR}/pr_bench.txt"
